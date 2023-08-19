33559ced82b4bd348b830a3a39a4f32a45380503
[SW5] 고윤렬 <sryr0810@gmail.com>
2023-08-20 04:23:32 +0900

    Feat: cookie 송수신 셋업

--- src/app.js
+++ src/app.js
@@ -24,13 +24,16 @@ const connectToDatabase = async (url) => {
 };
 
 const url = MONGODB_URI;
+
+const corsOption = { origin: 'http://localhost:3000', credentials: true };
+
 connectToDatabase(url);
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors(corsOption));
 app.use(cookieParser());
 
 app.use('/auth', authRouter);

7abc23e29c9cbf1710384565e7a7f6377dab913a
[SW5] 김진규 <rlawlsrb16736@gmail.com>
2023-08-20 11:37:44 +0900

    Feat: review model 작성

--- src/data-access/schema/reviewSchema.js
+++ src/data-access/schema/reviewSchema.js
@@ -2,14 +2,13 @@ const Mongoose = require('mongoose');
 
 const reviewSchema = new Mongoose.Schema(
     {
-        id: { type: Number, require: true },
         grade: { type: String, require: true },
         content: { type: String, require: true },
         usernickname: { type: String, require: true },
         username: { type: String, require: true },
         storeId: { type: String, require: true },
     },
-    { timestamps: true }, // createdAt
+    { timestamps: true, collection: 'review' }, // createdAt
 );
 
 module.exports = reviewSchema;
--- src/services/review.js
+++ src/services/review.js
@@ -75,3 +75,87 @@ module.exports = {
     getByStoreId,
     create,
 };
+
+// 임시 데이터
+// let reviews = [
+//     {
+//         id: '1',
+//         grade: '맛있다',
+//         content: '치즈피자가 제일 맛있어요!',
+//         usernickname: 'elice',
+//         username: 'Elice',
+//         createdAt: Date.now().toString(),
+//         storeId: '2',
+//     },
+//     {
+//         id: '2',
+//         grade: '괜찮다',
+//         content: '불고기피자 괜찮아요~',
+//         usernickname: 'bob',
+//         username: 'Bob',
+//         createdAt: Date.now().toString(),
+//         storeId: '3',
+//     },
+//     {
+//         id: '3',
+//         grade: '별로다',
+//         content: '야채피자 별로에요..',
+//         usernickname: 'james',
+//         username: 'James',
+//         createdAt: Date.now().toString(),
+//         storeId: '3',
+//     },
+// ];
+
+// async function getAll() {
+//     return reviews;
+// }
+
+// async function getByUserNickname(usernickname) {
+//     return reviews.filter((review) => review.usernickname === usernickname);
+// }
+
+// async function getByReviewId(reviewId) {
+//     return reviews.find((reviewItem) => reviewItem.id === reviewId);
+// }
+
+// async function update(reviewId, grade, content) {
+//     const review = reviews.find((reviewItem) => reviewItem.id === reviewId);
+//     if (review) {
+//         review.grade = grade;
+//         review.content = content;
+//     }
+//     return review;
+// }
+
+// async function remove(reviewId) {
+//     reviews = reviews.filter((review) => review.id !== reviewId);
+// }
+
+// async function getByStoreId(storeId) {
+//     return reviews.filter((review) => review.storeId === storeId);
+// }
+
+// async function create(storeId, grade, content, usernickname, username) {
+//     const newReview = {
+//         id: Date.now().toString(),
+//         grade,
+//         content,
+//         usernickname,
+//         username,
+//         createdAt: Date.now().toString(),
+//         storeId,
+//     };
+//     reviews = [newReview, ...reviews];
+//     return newReview;
+// }
+
+// module.exports = {
+//     getAll,
+//     getByUserNickname,
+//     getByReviewId,
+//     update,
+//     remove,
+//     getByStoreId,
+//     create,
+// };

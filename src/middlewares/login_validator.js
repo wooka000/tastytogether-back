1f568eba677778791e9ec6bec20b0ddfcaad861a
[SW5] 고윤렬 <sryr0810@gmail.com>
2023-08-20 15:05:38 +0900

    Feat: 로그인 인증 미들웨어 작성

--- src/middlewares/login_validator.js
+++ src/middlewares/login_validator.js
@@ -8,17 +8,18 @@ const verifyLogin = (req, res, next) => {
     if (req.method === 'OPTIONS') {
         next();
     }
+
+    const token = req.headers.authorization.split(' ')[1]; // Authorization: 'Bearer TOKEN'
+
+    if (!token) {
+        throw new Error('토큰이 존재하지 않습니다.');
+    }
     try {
-        const token = req.headers.authorization.split(' ')[1]; // Authorization: 'Bearer TOKEN'
-        if (!token) {
-            throw new Error('로그인되지 않았습니다.');
-        }
         const decodedToken = jwt.verify(token, ACCESS_TOKEN_SECRET);
-        console.log(decodedToken);
-        req.userId = { decodedToken };
+        req.user = { userId: decodedToken._id, email: decodedToken.email };
         next();
     } catch (error) {
-        error.message = '로그인 인증 실패하였습니다';
+        error.message = '로그인 인증 실패하였습니다.';
         next(error);
     }
 };

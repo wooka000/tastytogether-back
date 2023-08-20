c7e49443041e73c3f6e67b0d5005264b43291c19
[SW5] 고윤렬 <sryr0810@gmail.com>
2023-08-20 13:54:07 +0900

    Feat: login_validator 일부 수정

--- src/middlewares/login_validator.js
+++ src/middlewares/login_validator.js
@@ -6,19 +6,20 @@ const { ACCESS_TOKEN_SECRET } = process.env;
 
 const verifyLogin = (req, res, next) => {
     if (req.method === 'OPTIONS') {
-        return next();
+        next();
     }
     try {
         const token = req.headers.authorization.split(' ')[1]; // Authorization: 'Bearer TOKEN'
         if (!token) {
-            throw new Error('Authentication failed!');
+            throw new Error('로그인되지 않았습니다.');
         }
         const decodedToken = jwt.verify(token, ACCESS_TOKEN_SECRET);
         console.log(decodedToken);
         req.userId = { decodedToken };
         next();
     } catch (error) {
-        return next(new Error('Authentication failed!'));
+        error.message = '로그인 인증 실패하였습니다';
+        next(error);
     }
 };
 

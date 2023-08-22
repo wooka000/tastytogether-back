b246912e636ce86bae2d1bce8b742883d4a14744
[SW5] 고윤렬 <sryr0810@gmail.com>
2023-08-22 22:22:24 +0900

    Feat: Multer S3 미들웨어 작업 완성 및 리뷰 수정

--- src/middlewares/loginValidator.js
+++ src/middlewares/loginValidator.js
@@ -4,20 +4,15 @@ const jwt = require('jsonwebtoken');
 
 const { ACCESS_TOKEN_SECRET } = process.env;
 
-const verifyLogin = (req, res, next) => {
-    // preflight 요청에 대해선 verify 하지 않음.
-    // if (req.method === 'OPTIONS') {
-    //     next();
-    // }
-
-    const accessToken = req.headers.authorization.split(' ')[1]; // Authorization: 'Bearer TOKEN'
+const verifyLogin = async (req, res, next) => {
+    const accessToken = await req.headers.authorization.split(' ')[1]; // Authorization: 'Bearer TOKEN'
 
     if (!accessToken) {
         throw new Error('토큰이 존재하지 않습니다.');
     }
     try {
         const decodedToken = jwt.verify(accessToken, ACCESS_TOKEN_SECRET);
-        req.user = { userId: decodedToken._id, email: decodedToken.email };
+        req.userData = { userId: decodedToken._id, email: decodedToken.email };
         next();
     } catch (error) {
         throw new Error('로그인 인증 실패하였습니다.');

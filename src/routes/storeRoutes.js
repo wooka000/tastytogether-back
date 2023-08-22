9acedf6b2410de808bc439fe1e47110a503b44c0
Kimsuyeon <songudeum@gmail.com>
2023-08-22 18:56:57 +0900

    Comment: 가게 중복 확인 api 및 가게 등록 api 주석 추가 및 미작성된 라우터 경로 주석처리

--- src/routes/storeRoutes.js
+++ src/routes/storeRoutes.js
@@ -7,8 +7,8 @@ router.get('/', storeService.checkStore);
 // 새로운 업체 등록
 router.post('/', storeService.createStore);
 // 가게 검색
-router.get('/search?q=keyword', storeService.searchStores);
-// 맛집찾기 필터(업종, 지역)
-router.get('/search?type=type&region=city/state', storeService.filterStores);
+// router.get('/search?q=keyword', storeService.searchStores);
+// // 맛집찾기 필터(업종, 지역)
+// router.get('/search?type=type&region=city/state', storeService.filterStores);
 
 module.exports = router;

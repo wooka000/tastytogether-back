950a2513190428d317ca62260e22d370081b02ed
wooka000 <wooka000@naver.com>
2023-08-24 00:17:45 +0900

    Refactor: user review 관련 함수 리팩토링

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,31 +1,28 @@
 const { Router } = require('express');
-const mypageController = require('../services/user');
+const userController = require('../services/user');
 
 const router = Router();
 
-// 프로필 수정 api
-router.patch('/:userId', mypageController.editUser);
+// 프로필 수정 api O
+router.patch('/:userId', userController.editUser);
 
-// 유저 탈퇴 api
-router.delete('/:userId', mypageController.deleteUser);
+// 유저 탈퇴 api O
+router.delete('/:userId', userController.deleteUser);
 
-// 게시글 목록 조회 api => /board/:userId
-router.get('/:userid/boards', mypageController.getBoards);
+// 리뷰 목록 조회 api => login 미들웨어 확인
+router.get('/reviews', userController.getMyReviews);
 
-// 리뷰 목록 조회 api => /user/reviews
-router.get('/reviews', mypageController.getReviews);
-
-// 리뷰 수정 api => /review/:reviewId
-router.patch('/reviews/:reviewid', mypageController.editReview);
-
-// 리뷰 삭제 api => /review/:reviewId
-router.delete('/reviews/:reviewid', mypageController.deleteReview);
+// 특정 사용자가 작성한 리뷰들 조회 O
+router.get('/:userId/reviews', userController.getUserReviews);
 
 // 가게 찜 목록 조회 api => /store/likes/:userId
-router.get('/:userid/storelikes', mypageController.getStoreLikes);
+router.get('/:userid/storelikes', userController.getStoreLikes);
 
 // 가게 찜 삭제 api => /store/like/:userId
-router.delete('/:userid/storelikes/:storeid', mypageController.deleteStoreLike);
+router.delete('/:userid/storelikes/:storeid', userController.deleteStoreLike);
+
+// 게시글 목록 조회 api => /board/:userId X
+router.get('/:userid/boards', userController.getBoards);
 
 // 추가) 라이크 후순위 작업
 

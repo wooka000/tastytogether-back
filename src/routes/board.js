a3f04803e54edeba461a5f53e95ea6bcbb9d8534
[sw5] 소화경 <dsjs9912@gmail.com>
2023-08-24 05:18:58 +0900

    Feat: 페이지네이션,검색기능 추가

--- src/routes/board.js
+++ src/routes/board.js
@@ -4,7 +4,12 @@ const router = Router();
 const boardController = require('../services/board');
 
 // 게시글 목록 조회
-router.get('/posts', boardController.getAllBoard);
+router.get('/posts', (req, res) => {
+    const countPerPage = parseInt(req.query.countperpage, 10) || 10;
+    const pageNo = parseInt(req.query.pageno, 10) || 1;
+
+    boardController.getAllBoard(req, res, countPerPage, pageNo);
+});
 
 // 게시글 상세 조회
 // router.get('/posts/:id', boardController.getOneBoard);
@@ -12,13 +17,17 @@ router.get('/posts', boardController.getAllBoard);
 // 게시글 작성
 router.post('/posts', boardController.postBoard);
 
+// 게시글 상세 정보 조회
+router.get('/posts/:id', boardController.getDetailBoard);
+
 // 게시글 수정
 router.patch('/posts/:id', boardController.editBoard);
 
 // 게시글 삭제
 router.delete('/posts/:id', boardController.deleteBoard);
 
-// 게시글 상세 정보 조회
-router.get('/posts/:id', boardController.getDetailBoard);
+
+
+router.get('/regionSearch', boardController.getSearchBoard);
 
 module.exports = router;

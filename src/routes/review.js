54579e1b4d159f85b92335d5c4a0b1278f7ef991
wooka000 <wooka000@naver.com>
2023-08-25 16:08:01 +0900

    Feat: 이미지 처리 관련 로직

--- src/routes/review.js
+++ src/routes/review.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const { uploadMultiImage } = require('../middlewares/imageUploader');
 const verifyLogin = require('../middlewares/loginValidator');
 const reviewController = require('../services/review');
 
@@ -8,12 +9,12 @@ const router = Router();
 router.get('/:reviewId', reviewController.getReviewById);
 
 // 리뷰 생성 api O
-router.post('/:storeId', verifyLogin, reviewController.createReview);
+router.post('/:storeId', verifyLogin, uploadMultiImage('photos'), reviewController.createReview);
 
 // 리뷰 수정 api O
-router.patch('/:reviewId', verifyLogin, reviewController.editReview);
+router.patch('/:reviewId', verifyLogin, uploadMultiImage('photos'), reviewController.editReview);
 
 // 리뷰 삭제 api O
-router.delete('/:reviewId', verifyLogin, reviewController.deleteReview);
+router.delete('/:reviewId', verifyLogin, uploadMultiImage('photos'), reviewController.deleteReview);
 
 module.exports = router;
---
+++ src/utils/photoLimit.js
@@ -0,0 +1,3 @@
+const photoLimit = (arr) => arr.length <= 8;
+
+module.exports = photoLimit;

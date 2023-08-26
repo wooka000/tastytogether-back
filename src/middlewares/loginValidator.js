8d13246ae74ad383e49fe9acc16ed83fb3863bf6
[SW5] 고윤렬 <sryr0810@gmail.com>
2023-08-26 10:39:43 +0900

    Fix: 엑세스 토큰 시간 10분

--- src/middlewares/loginValidator.js
+++ src/middlewares/loginValidator.js
@@ -8,14 +8,14 @@ const verifyLogin = async (req, res, next) => {
     const accessToken = await req.headers.authorization.split(' ')[1]; // Authorization: 'Bearer TOKEN'
 
     if (!accessToken) {
-        throw new Error('토큰이 존재하지 않습니다.');
+        next(new Error('토큰이 존재하지 않습니다.'));
     }
     try {
         const decodedToken = jwt.verify(accessToken, ACCESS_TOKEN_SECRET);
         req.userData = { userId: decodedToken._id, email: decodedToken.email };
         next();
     } catch (error) {
-        throw new Error('로그인 인증 실패하였습니다.');
+        next(new Error('로그인 인증 실패하였습니다.'));
     }
 };
 
